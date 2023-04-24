2f21054834ae0a7f464e37072cb1848572a890d2
TetaSub <tetyana.subbotina@gmail.com>
2023-04-24 13:13:29 +0100

    local storage functions updated, edit functions added

--- src/index.js
+++ src/index.js
@@ -23,18 +23,48 @@ const storeTaskInLocalStorage = (task) => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
 };
 
-const removeTaskFromLocalStorage = (deletedTask) => {
-  const tasks = getTasksFromLocalStorage();
 
-  // other variant .filter
-  const deletedIndex = tasks.findIndex((task) => task === deletedTask);
-  tasks.splice(deletedIndex, 1);
+const storeEditedTaskInLocalStorage = (editedTask) => {
+  let tasks = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const editedTaskIndex = tasks.findIndex(task => task.taskId === Number(editedTask.taskId));
+  tasks[editedTaskIndex].taskContent = editedTask.taskContent;
+  
+  // other possible solution (but not working)
+  //tasks = tasks.filter((task) => task.taskId !== Number(editedTask.taskId));
+  //tasks.taskContent = editedTask.taskContent; 
+  
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+};
+
+const removeTaskFromLocalStorage = (taskId) => {
+  let tasks = getTasksFromLocalStorage();
+
+  tasks = tasks.filter((task) => task.taskId !== Number(taskId));  
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
 };
 
 const clearTasksFromLocalStorage = () => {
   localStorage.removeItem(STORAGE_KEY);
+};
+
+// "icons" functions
+
+const createEditIcon = (span) => {
+  const editIcon = document.createElement("span");
+  editIcon.className = "edit-item";
+  editIcon.innerHTML = '<i class="fa fa-edit"></i>';
+  editIcon.addEventListener("click", editTask);
+  span.append(editIcon);
+};
+
+const createDeleteIcon = (span) => {
+  const deleteIcon = document.createElement("span");
+  deleteIcon.className = "delete-item";
+  deleteIcon.innerHTML = '<i class="fa fa-remove"></i>';
+  deleteIcon.addEventListener("click", removeTask);
+  span.append(deleteIcon);
 };
 
 // "tasks" functions
@@ -44,12 +74,17 @@ const getTasks = () => {
   tasks.forEach((task) => {
     const li = document.createElement("li");
     li.className = "collection-item";
-    li.textContent = task;
+    const taskId = task.taskId;
+    li.setAttribute("data-task-id", taskId);
+    li.textContent = task.taskContent;
 
-    const taskText = document.createElement("span");
-    taskText.className = "delete-item";
-    taskText.innerHTML = '<i class="fa fa-remove"></i>';
-    li.append(taskText);
+    // add icons
+    const taskIcons = document.createElement("span");
+    taskIcons.className = "icon-item";
+    
+    createEditIcon(taskIcons);
+    createDeleteIcon(taskIcons);
+    li.append(taskIcons);
 
     // Append li to ul
     taskList.append(li);
@@ -67,22 +102,57 @@ const addTask = (event) => {
   // Create and add LI element
   const li = document.createElement("li");
   li.className = "collection-item";
-  li.textContent = taskInput.value; // значення яке ввів користувач
+  const taskId = Date.now();
+  li.setAttribute("data-task-id", taskId);
+  li.textContent = taskInput.value; // значення яке ввів користувач  
 
-  const taskText = document.createElement("span");
-  taskText.className = "delete-item";
-  taskText.innerHTML = '<i class="fa fa-remove"></i>';
-  li.append(taskText);
+  // add icons
+  const taskIcons = document.createElement("span");
+  taskIcons.className = "icon-item";
+  createEditIcon(taskIcons);
+  createDeleteIcon(taskIcons);
+  li.append(taskIcons);
 
   taskList.append(li);
 
   // Save to storage
-  storeTaskInLocalStorage(taskInput.value);
+  const taskContent = taskInput.value;
+  const storedTask = { taskContent, taskId };
+  
+  storeTaskInLocalStorage(storedTask);
 
   // Clear input value
   taskInput.value = "";
 };
 
+
+// edit task function
+const editTask = (event) => {
+  const editedLi = event.target.closest("li");
+  if (editedLi) {
+    const newTaskText = prompt(
+      "Відредагуйте завданя:",
+      editedLi.textContent.trim()
+    );
+
+    if (newTaskText !== null && newTaskText.trim() !== "") {
+      editedLi.textContent = newTaskText.trim();
+      const taskId = editedLi.getAttribute("data-task-id");
+
+      const taskIcons = document.createElement("span");
+      taskIcons.className = "icon-item";
+      createEditIcon(taskIcons);
+      createDeleteIcon(taskIcons);
+      editedLi.append(taskIcons);      
+
+      const editedTask = { taskContent: newTaskText.trim(), taskId };
+      storeEditedTaskInLocalStorage(editedTask);
+    }
+  }
+};
+
+
+// remove task function
 const removeTask = (event) => {
   const isDeleteIcon = event.target.classList.contains("fa-remove");
 
@@ -90,12 +160,11 @@ const removeTask = (event) => {
     const isApproved = confirm("Ви впевнені що хочете видалити це завдання?");
 
     if (isApproved) {
-      // remove from DOM
-      // console.log(event.target.parentElement.parentElement);
       const deletedLi = event.target.closest("li");
+      const taskId = deletedLi.getAttribute("data-task-id");
       deletedLi.remove();
 
-      removeTaskFromLocalStorage(deletedLi.textContent);
+      removeTaskFromLocalStorage(taskId);
     }
   }
 };
@@ -105,6 +174,9 @@ const clearTasks = () => {
   clearTasksFromLocalStorage();
 };
 
+
+// filter functions
+
 const filterTasks = (event) => {
   const text = event.target.value.toLowerCase();
   const list = document.querySelectorAll(".collection-item");
@@ -113,27 +185,44 @@ const filterTasks = (event) => {
     const item = task.firstChild.textContent.toLowerCase();
 
     if (item.includes(text)) {
-      // task.style.display = "block"; // task.hidden = true
-      task.style.display = "list-item";
+      task.style.display = "flex";
     } else {
       task.style.display = "none";
     }
   });
 };
 
+const clearFilter = () => {
+  filterInput.value = '';
+  filterTasks({target: {value: ''}});
+}
+
+
+
 // init
 getTasks();
 
 // Event listeners
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   getTasks();
-// });
-
 form.addEventListener("submit", addTask);
 
-taskList.addEventListener("click", removeTask);
+form.addEventListener("submit", clearFilter);
+
+/*taskList.addEventListener("click", (event) => {
+  const isDeleteIcon = event.target.classList.contains("fa-remove");
+  const isEditIcon = event.target.classList.contains("fa-edit");
+
+  if (isDeleteIcon) {
+    removeTask(event);
+  } else if (isEditIcon) {
+    editTask(event);
+  }
+});*/
 
 clearButton.addEventListener("click", clearTasks);
 
+clearButton.addEventListener("click", clearFilter);
+
 filterInput.addEventListener("input", filterTasks);
+
+taskInput.addEventListener("focus", clearFilter);
